a7d7f90908d1a643398ad324c3e96cac668c77c5
Luis <lfgranjaead@gmail.com>
2026-03-17 10:48:38 -0400

    anagram exercise

--- rust/anagram/src/main.rs
+++ rust/anagram/src/main.rs
@@ -2,7 +2,9 @@ use anagram::anagrams_for;
 
 fn main() {
     let word = "stone";
-    let possible_anagrams = &["stone", "tones", "banana", "tons", "notes", "Seton"];
+    let possible_anagrams = &[
+        "stone", "tones", "banana", "tons", "notes", "Seton", "Soten", "steNo", "sonet",
+    ];
     let anagrams = anagrams_for(word, possible_anagrams);
     println!("Anagrams for '{word}': {anagrams:?}");
 }
