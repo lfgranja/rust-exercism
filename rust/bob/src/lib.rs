aab553194b251392b24621fadcebfbcab97a415f
Luis <lfgranjaead@gmail.com>
2026-03-29 18:25:57 -0400

    Bob Exercism

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,25 +1,3 @@
-//! rust/bob/src/lib.rs
-
-/*
-Introduction
-Bob is a lackadaisical teenager. He likes to think that he's very cool. And he definitely doesn't get excited about things. That wouldn't be cool.
-
-When people talk to him, his responses are pretty limited.
-
-Instructions
-Your task is to determine what Bob will reply to someone when they say something to him or ask him a question.
-
-Bob only ever answers one of five things:
-
-"Sure." This is his response if you ask him a question, such as "How are you?" The convention used for questions is that it ends with a question mark.
-"Whoa, chill out!" This is his answer if you YELL AT HIM. The convention used for yelling is ALL CAPITAL LETTERS.
-"Calm down, I know what I'm doing!" This is what he says if you yell a question at him.
-"Fine. Be that way!" This is how he responds to silence. The convention used for silence is nothing, or various combinations of whitespace characters.
-"Whatever." This is what he answers to anything else.
-Source
-Inspired by the 'Deaf Grandma' exercise in Chris Pine's Learn to Program tutorial.The link opens in a new window or tab
-*/
-
 pub fn reply(message: &str) -> &str {
     match message.trim() {
         "" => "Fine. Be that way!",
