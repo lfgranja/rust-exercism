126dca74579b7e0755767da25ba8f7fea33623a9
Luis <lfgranjaead@gmail.com>
2026-03-29 00:18:33 -0400

    Prime factors exercism

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,55 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+//! prime-factors/src/lib.rs
+
+/*
+ *
+Instructions
+ Compute the prime factors of a given natural number.
+
+ A prime number is only evenly divisible by itself and 1.
+
+ Note that 1 is not a prime number.
+
+ Example
+ What are the prime factors of 60?
+
+ Our first divisor is 2. 2 goes into 60, leaving 30.
+ 2 goes into 30, leaving 15.
+ 2 doesn't go cleanly into 15. So let's move on to our next divisor, 3.
+ 3 goes cleanly into 15, leaving 5.
+ 3 does not go cleanly into 5. The next possible factor is 4.
+ 4 does not go cleanly into 5. The next possible factor is 5.
+ 5 does go cleanly into 5.
+ We're left only with 1, so now, we're done.
+ Our successful divisors in that computation represent the list of prime factors of 60: 2, 2, 3, and 5.
+
+ You can check this yourself:
+
+ 2 * 2 * 3 * 5
+ = 4 * 15
+ = 60
+ Success!
+ */
+
+use std::iter::from_fn;
+
+pub fn factors(mut remaining: u64) -> Vec<u64> {
+    let mut divisor = 2;
+
+    from_fn(move || {
+        if remaining <= 1 {
+            return None;
+        }
+
+        while divisor * divisor <= remaining {
+            if remaining.is_multiple_of(divisor) {
+                remaining /= divisor;
+                return Some(divisor);
+            }
+            divisor += if divisor == 2 { 1 } else { 2 };
+        }
+        let last_factor = remaining;
+        remaining = 1;
+        Some(last_factor)
+    })
+    .collect()
 }
